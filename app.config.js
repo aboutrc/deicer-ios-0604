327ba67f7f45d51ba7d07208b762839e91a69f73
aboutrc <rc@aboutrc.com>
2025-06-04 17:02:45 -0400

    Initial commit with full project setup

---
+++ app.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: "DEICER",
+  slug: "DEICER",
+  owner: "aboutrc",
+  projectId: "be205279-baa0-431e-8c0b-8af518b6ebee",
+  version: "1.0.0",
+  ios: {
+    bundleIdentifier: "com.deicer.mobile"
+  },
+  extra: {
+    eas: {
+      projectId: "be205279-baa0-431e-8c0b-8af518b6ebee"
+    }
+  }
+}; 
--- app.config.ts
+++ app.config.ts
@@ -2,10 +2,42 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'Deicer',
-  slug: 'deicer',
+  name: 'DEICER',
+  slug: 'DEICER',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icons/icon.png',
+  userInterfaceStyle: 'automatic',
+  splash: {
+    image: './assets/images/icons/ios-splash-icon-light.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff'
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
   extra: {
     googleTranslateApiKey: process.env.EXPO_PUBLIC_GOOGLE_TRANSLATE_API_KEY,
-    openaiApiKey: process.env.EXPO_PUBLIC_OPENAI_API_KEY,
+    openaiApiKey: process.env.EXPO_PUBLIC_OPENAI_API_KEY
   },
+  plugins: [
+    "expo-router"
+  ],
+  scheme: "DEICER",
+  web: {
+    bundler: "metro",
+    favicon: './assets/images/favicon.png'
+  },
+  ios: {
+    bundleIdentifier: "com.deicer.mobile",
+    supportsTablet: true,
+    icon: './assets/images/icons/ios-light.png'
+  },
+  android: {
+    package: "com.deicer.mobile",
+    adaptiveIcon: {
+      foregroundImage: './assets/images/icons/adaptive-icon.png',
+      backgroundColor: '#ffffff'
+    }
+  }
 });
